d99ff15f43c0318a698f0a8c198224dbda2ff099
JiajunJerryHuang <huangjiajun2020@sina.com>
2023-11-06 14:46:29 -0800

    post and recorded page changed to react

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./pages/login_signup";
+import PostPage from "./pages/Post";
+import RecordedPage from "./pages/Recorded";
+import EditPage from "./pages/Edit";
 import './App.css';
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LoginForm />} />
+          <Route path="/PostPage" element = {<PostPage />}/>
+          <Route path="/RecordedPage" element = {<RecordedPage />}/>
+          <Route path="/EditPage" element = {<EditPage />}/>
         </Routes>
       </Router>
     </div>
