6eb11e3bf3c8c315484c9829687c48aadbc9fd1e
Jasesssss <17321255681@163.com>
2023-11-26 19:12:11 -0800

    Update

--- app/backend/router/postRouter.js
+++ app/backend/router/postRouter.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const PostDao = require('../dao/displayDAO');
+const { getUsernameAndPostDate } = require('../dao/displayDAO'); // Update the path to the actual location of your PostDao file
+
 const router = express.Router();
 
-router.get('/post-history', (req, res) => {
-    const postDao = new PostDao(req.db);
-
-    postDao.getUsernameAndPostDate((err, results) => {
+// Endpoint to get username and post date based on user ID
+router.get('/post-history/:user_id', (req, res) => {
+    const userId = req.params;
+    console.log(userId)
+    getUsernameAndPostDate(userId, (err, results) => {
         if (err) {
+            console.error('Error in /post-history/:user_id:', err);
             res.status(500).send('Error retrieving data from database');
-            return;
+        } else {
+            res.json({data : results });
         }
-        res.json(results);
     });
 });
+
+// Additional routes can be defined here using postDao's methods
 
 module.exports = router;
--- app/backend/server.js
+++ app/backend/server.js
@@ -1,28 +1,32 @@
 const express = require('express');
-const mysql = require('mysql');
-require('dotenv').config({ path: "./process.env" });
+const cors = require('cors');
+// const mysql = require('mysql');
+// require('dotenv').config({ path: "./process.env" });
 const app = express();
 const port = 5000;
 
-const connection = mysql.createConnection({
-  host: process.env.DBHOST,
-  user: process.env.DBUSER,
-  password: process.env.DBPASS,
-  database: process.env.DBNAME
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DBHOST,
+//   user: process.env.DBUSER,
+//   password: process.env.DBPASS,
+//   database: process.env.DBNAME
+// });
 
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
-
+// connection.connect(error => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the database.");
+// });
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 
 const postRouter = require('./router/postRouter');
 app.use(postRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const recordedRouter = require('./router/recordedRouter');
+app.use(recordedRouter);
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
