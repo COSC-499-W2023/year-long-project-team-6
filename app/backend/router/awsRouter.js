a6cc554d7eb0de1899dbef5b5a06e8d739be398b
davyxuximin <davyxuximin@sina.com>
2023-12-03 20:20:43 -0800

    kvs store in s3 finished

--- app/backend/router/awsRouter.js
+++ app/backend/router/awsRouter.js
@@ -1,57 +1,75 @@
-// router/awsRouter.js
-
-const express = require('express');
-const AWS = require('aws-sdk');
-const router = express.Router();
 require('dotenv').config({ path: "./process.env" });
-AWS.config.update({region: 'us-east-1'});
+process.env.AWS_SDK_LOAD_CONFIG = '1'; // Enable loading of AWS SDK config
+process.env.AWS_PROFILE = 'botocore-client-ximin_sso'; 
+const { KinesisVideo } = require('@aws-sdk/client-kinesis-video');
+const express = require('express');
+const { STSClient, AssumeRoleCommand } = require('@aws-sdk/client-sts');
+const fs = require('fs');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // temporarily store files in 'uploads' folder
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
-// Configure AWS
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    sessionToken: process.env.AWS_SESSION_TOKEN,
-    region: 'us-east-1' // Change to your AWS region
+// Initialize the S3 client
+const s3Client = new S3Client({ region: process.env.AWS_REGION || 'us-east-1' });
+
+// Initialize Kinesis Video Client
+const kinesisVideoClient = new KinesisVideo({
+    region: process.env.AWS_REGION || 'us-east-1',
+    // other configurations if needed
 });
-console.log("Updated AWS Config:", AWS.config.credentials.accessKeyId, AWS.config.region);
-const sts = new AWS.STS();
 
+// Function to obtain temporary AWS credentials
 async function getTemporaryCredentials() {
-    return sts.assumeRole({
-        RoleArn: 'arn:aws:iam::466618866658:role/web-rtc-499', // replace with your role's ARN
-        RoleSessionName: 'WebClientSession' // This is an identifier for the session
-    }).promise();
-};
+    const stsClient = new STSClient({ region: 'us-east-1' });
+    const assumeRoleCommand = new AssumeRoleCommand({
+        RoleArn: 'arn:aws:iam::466618866658:role/web-rtc-499',
+        RoleSessionName: 'WebClientSession',
+        // Add other required parameters if necessary
+    });
 
-// Express.js route handler to provide temporary credentials to the client
+    try {
+        const data = await stsClient.send(assumeRoleCommand);
+        return data.Credentials;
+    } catch (error) {
+        console.error('Error assuming role:', error);
+        throw error;
+    }
+}
+
+// Route handler to provide temporary credentials to the client
 router.get('/get-temp-credentials', async (req, res) => {
     try {
-        const data = await getTemporaryCredentials();
-        res.json({
-            accessKeyId: data.Credentials.AccessKeyId,
-            secretAccessKey: data.Credentials.SecretAccessKey,
-            sessionToken: data.Credentials.SessionToken,
-            expiration: data.Credentials.Expiration // send expiration time as well
-        });
+        const credentials = await getTemporaryCredentials();
+        if (credentials) {
+            res.json({
+                accessKeyId: credentials.AccessKeyId,
+                secretAccessKey: credentials.SecretAccessKey,
+                sessionToken: credentials.SessionToken,
+                expiration: credentials.Expiration
+            });
+        } else {
+            throw new Error('No credentials returned from assume role operation');
+        }
     } catch (error) {
         console.error('Error assuming role:', error);
         res.status(500).json({ error: 'Error obtaining temporary credentials', details: error.toString() });
-        
     }
 });
 
+// Route handler to provide signaling channel configuration to the client
 router.get('/getSignalingChannelConfig', async (req, res) => {
     const channelARN = req.query.channelARN;
-    const kinesisVideoClient = new AWS.KinesisVideo();
+    
     try {
-        // Get signaling channel endpoint
-        const endpointResponse = await kinesisVideoClient.getSignalingChannelEndpoint({
+        const params = {
             ChannelARN: channelARN,
             SingleMasterChannelEndpointConfiguration: {
                 Protocols: ['WSS', 'HTTPS'],
                 Role: 'VIEWER',
             },
-        }).promise();
+        };
+        const endpointResponse = await kinesisVideoClient.getSignalingChannelEndpoint(params);
 
         // Extract the endpoints
         const endpointsByProtocol = endpointResponse.ResourceEndpointList.reduce((endpoints, endpoint) => {
@@ -59,12 +77,39 @@ router.get('/getSignalingChannelConfig', async (req, res) => {
             return endpoints;
         }, {});
 
-        // Send the response back to the client
         res.json({ endpointsByProtocol });
     } catch (error) {
         console.error('Error fetching signaling channel config:', error);
         res.status(500).json({ error: error.message });
     }
 });
+router.post('/upload-video', upload.single('video'), async (req, res) => {
+    try {
+        const file = req.file;
+        const bucketName = 'cosc499-video-submission'; // Replace with your S3 bucket name
+        const key = `videos/${Date.now()}_${file.originalname}`;
+
+        // Read the file from the local file system
+        const fileStream = fs.createReadStream(file.path);
+
+        const uploadParams = {
+            Bucket: bucketName,
+            Key: key,
+            Body: fileStream,
+            ContentType: file.mimetype,
+        };
+
+        const data = await s3Client.send(new PutObjectCommand(uploadParams));
+
+        // Delete the file from local storage after uploading
+        fs.unlinkSync(file.path);
+
+        res.status(200).json({ message: 'Video uploaded successfully', data });
+    } catch (error) {
+        console.error('Error uploading video:', error);
+        res.status(500).json({ error: 'Error uploading video', details: error.toString() });
+    }
+});
+
 
 module.exports = router;
--- app/backend/server.js
+++ app/backend/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config({ path: "./process.env"});
+
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 
 const app = express();
 const port = 5001;
-
 
 app.use(cors({
   origin: 'http://localhost:3000',
@@ -31,9 +32,12 @@ app.use(recordedRouter);
 const groupRouter = require('./router/groupRouter');
 app.use(groupRouter);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
+const server = http.createServer(app);
+
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
 
