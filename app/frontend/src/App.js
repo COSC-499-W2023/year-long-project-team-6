b8ad77a577c229fb1f257b360eafd466d65bd4de
Jasesssss <17321255681@163.com>
2024-01-21 21:21:43 -0800

    Debug for Session

---
+++ app/frontend/src/App.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+import LoginForm from "./pages/login_signup";
+import PostPage from "./pages/Post";
+import RecordedPage from "./pages/Recorded";
+import EditPage from "./pages/Edit";
+import MainContent from './pages/MainContent';
+import Sidebar from './pages/sidebar';
+import Profile from './pages/UserProfile';
+import Members from './pages/Members';
+import './component/CSS/style.css'; 
+import './component/CSS/sidebar_style.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/" element={<LayoutWithSidebar />}>
+              <Route index element={<MainContent />} />
+              <Route path="PostPage" element={<PostPage />} />
+              <Route path="RecordedPage" element={<RecordedPage />} />
+              <Route path="EditPage" element={<EditPage />} />
+              <Route path="Profile" element={<Profile />} />
+              <Route path="Members" element={<Members />} />
+            </Route>
+          </Routes>
+        </Router>
+    </div>
+  );
+}
+function LayoutWithSidebar() {
+  return (
+    <div>
+      <Sidebar />
+      <Outlet /> {/* Renders the child routes */}
+    </div>
+  );
+}
+
+
+export default App;
