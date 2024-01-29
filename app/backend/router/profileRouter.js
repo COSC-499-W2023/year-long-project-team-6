fe07b81c9b1393d440ff47e0648f757c4eae006d
davyxuximin <davyxuximin@sina.com>
2024-01-29 15:18:11 -0800

    profile role deleted

--- app/backend/router/profileRouter.js
+++ app/backend/router/profileRouter.js
@@ -6,9 +6,9 @@ router.put('/edit-profile/:userId', (req, res) => {
     console.log('Received data:', req.body);
     const userId = req.params.userId;
     const userData = req.body; // Assuming the array is sent directly
-    // console.log("Updated Profile: " + updatedProfile);
     editUserProfile(userId, userData, (err, result) => {
         if (err) {
+            console.log("error",err);
             res.status(500).send('Error updating profile: ' + err.message);
         } else {
             res.status(200).send('Profile updated successfully');
@@ -21,6 +21,7 @@ router.get('/get-profile/:userId', (req, res) => {
     const userId = req.params.userId;
     getUserProfile(userId, (err, result) => {
         if (err) {
+            console.log("error",err);
             res.status(500).send('Error fetching profile: ' + err.message);
         } else if (result) {
             res.status(200).json(result);
