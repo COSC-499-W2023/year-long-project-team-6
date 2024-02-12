ecacca0a7e63088772aeb044ae4f51ade83d458d
skywalker007-cpu <656749873@qq.com>
2024-02-11 22:46:29 -0800

    avatar show beside username in the member list

--- app/frontend/src/pages/Members.js
+++ app/frontend/src/pages/Members.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams, Link  } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import '../component/CSS/MembersPage.css';
+import testPicture from '../component/image/flash.png';
 
 
 function MembersPage() {
@@ -10,8 +11,8 @@ function MembersPage() {
     const [members, setMembers] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [roleFilter, setRoleFilter] = useState('All Roles');
-    const [admin , setAdmin] = useState([])
-    const [adminid, setAdminid]=useState([])
+    const [admin, setAdmin] = useState([])
+    const [adminid, setAdminid] = useState([])
     console.log(groupId);
     useEffect(() => {
         const sessionUser = sessionStorage.getItem('user');
@@ -25,7 +26,7 @@ function MembersPage() {
     }, []);
     const navigateToGroupPostMember = (memberId) => () => {
         navigate(`/groupPost/${groupId}/${memberId}`);
-      };
+    };
 
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
@@ -46,11 +47,11 @@ function MembersPage() {
                     window.location.reload();
                 }
             )
-            .catch(
-                error => {
-                    console.error('Error:', error);
-                }
-            );
+                .catch(
+                    error => {
+                        console.error('Error:', error);
+                    }
+                );
         } catch (error) {
             console.error('Error removing user from group:', error);
         }
@@ -67,11 +68,11 @@ function MembersPage() {
                     navigate(`/`);
                 }
             )
-            .catch(
-                error => {
-                    console.error('Error:', error);
-                }
-            );
+                .catch(
+                    error => {
+                        console.error('Error:', error);
+                    }
+                );
         } catch (error) {
             console.error('Error deleting group:', error);
         }
@@ -81,7 +82,7 @@ function MembersPage() {
     const navigateToPostPage = () => {
         navigate('/PostPage', { state: { groupId } });
     };
-    
+
     useEffect(() => {
         if (userId) {
             fetch(`http://localhost:5001/groups-users/${groupId}`)
@@ -94,12 +95,20 @@ function MembersPage() {
                 .then(data => {
                     console.log(userId);
                     console.log('Fetched data:', data);
-                    setMembers(data);
+                    const updatedMembers = data.map(member => {
+                        if (member.user_image) {
+                            member.user_image = `data:image/*;base64,${member.user_image}`;
+                        } else {
+                            member.user_image = testPicture;
+                        }
+                        return member;
+                    });
+                    setMembers(updatedMembers);
                 })
                 .catch(error => {
                     console.error('Error fetching posts:', error);
                 });
-                fetch(`http://localhost:5001/group-admin/${groupId}`)
+            fetch(`http://localhost:5001/group-admin/${groupId}`)
                 .then(response => {
                     if (!response.ok) {
                         throw new Error(`HTTP error! status: ${response.status}`);
@@ -108,78 +117,83 @@ function MembersPage() {
                 })
                 .then(data => {
                     console.log(userId);
-                    console.log('groupadmin:', data); 
+                    console.log('groupadmin:', data);
                     console.log(data[0].username)
-                    setAdmin(data[0].username); 
+                    setAdmin(data[0].username);
                     setAdminid(data[0].admin);
-                    
+
                 })
                 .catch(error => {
                     console.error('Error fetching posts:', error);
                 });
         }
-            
+
     }, [userId]);
     console.log(roleFilter);
 
     return (
         <>
-        <div className="members-page">
-        <h3 id="admin">Admin: {admin}</h3>
-            <div className="members-filter">
-                <input
-                    type="text"
-                    placeholder="Search username"
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                    className="search-bar"
-                />
-                
-                <select value={roleFilter} onChange={handleRoleChange} className="dropdown">
-                    <option value="All Roles">All Roles</option>
-                    <option value="sender">Sender</option>
-                    <option value="Admin">Admin</option>
-                </select>
-                <span className='button'>
-                {userId != adminid ? (
-                             <button onClick={navigateToGroupPostMember(adminid)}>View Posts</button>
-                            ) :                              
+            <div className="members-page">
+                <h3 id="admin">Admin: {admin}</h3>
+                <div className="members-filter">
+                    <input
+                        type="text"
+                        placeholder="Search username"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                        className="search-bar"
+                    />
+
+                    <select value={roleFilter} onChange={handleRoleChange} className="dropdown">
+                        <option value="All Roles">All Roles</option>
+                        <option value="sender">Sender</option>
+                        <option value="Admin">Admin</option>
+                    </select>
+                    <span className='button'>
+                        {userId != adminid ? (
+                            <button onClick={navigateToGroupPostMember(adminid)}>View Posts</button>
+                        ) :
                             <button onClick={() => deleteGroup(groupId)}>Delete Group</button>
                         }
-                </span>
-                <button onClick={navigateToPostPage} className="navigate-post-page-button">Create Post</button>
-            </div>
-            <table className="members-table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Role</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {members
-                    .filter(member => 
-                        member.username.toLowerCase().includes(searchTerm.toLowerCase()) &&
-                        (roleFilter === 'All Roles' || member.role === roleFilter)
-                      )
-                    .map((member, index) => (
-                        <tr key={member.userid + '-' + index}>
-                            <td>{member.username}</td>
-                            <td>{member.userid == adminid ? 'Admin' : 'Sender'}</td>
-                            <td>
-                             {userId == adminid ? (
-                                <div className='adminButton'>
-                                    <button onClick={navigateToGroupPostMember(member.userid)}>View Posts</button>
-                                    <button onClick={() => removeUserFromGroup(member.userid)}>Delete User</button>
-                                </div>
-                            ) : null}
-            </td>
+                    </span>
+                    <button onClick={navigateToPostPage} className="navigate-post-page-button">Create Post</button>
+                </div>
+                <table className="members-table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Role</th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {members
+                            .filter(member =>
+                                member.username.toLowerCase().includes(searchTerm.toLowerCase()) &&
+                                (roleFilter === 'All Roles' || member.role === roleFilter)
+                            )
+                            .map((member, index) => (
+                                <tr key={member.userid + '-' + index}>
+                                    <td>
+                                        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                                            <img src={member.user_image} style={{ width: '30px', height: '30px', borderRadius: '50%' }} alt="avatar" />
+                                            <span>{member.username}</span>
+                                        </div>
+                                    </td>
+                                    <td>{member.userid == adminid ? 'Admin' : 'Sender'}</td>
+                                    <td>
+                                        {userId == adminid ? (
+                                            <div className='adminButton'>
+                                                <button onClick={navigateToGroupPostMember(member.userid)}>View Posts</button>
+                                                <button onClick={() => removeUserFromGroup(member.userid)}>Delete User</button>
+                                            </div>
+                                        ) : null}
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 
