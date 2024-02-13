c9dd1b630930f27575b9f598f9e809b35c59d5b9
WenruiChen <719941703@qq.com>
2024-02-13 13:04:04 -0800

    defaultAvatar

--- app/frontend/src/pages/Members.js
+++ app/frontend/src/pages/Members.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams, Link } from "react-router-dom";
 import '../component/CSS/MembersPage.css';
-import testPicture from '../component/image/flash.png';
+import testPicture from '../component/image/profileDefault.jpg';
 
 
 function MembersPage() {
